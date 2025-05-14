c7ca024be5a546b42b170ed073e50ee2c1605c19
PTDTW <113230263+PTDTW@users.noreply.github.com>
2025-05-14 20:46:04 +0800

    Update site URL in astro.config.mjs to the new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ptdtw.vercel.app/",
+	site: "https://www.ptdtw.fun/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
